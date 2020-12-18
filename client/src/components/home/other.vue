b85740c32ee76e396dd522f76513e2b03d5cbdcd
超 <wx_2200c03b49cd4eac9bd5246bca2e5580@git.code.tencent.com>
2020-12-18 18:14:31 +0800

    首页样式完成

--- client/src/components/home/other.vue
+++ client/src/components/home/other.vue
@@ -11,7 +11,48 @@
             <p>爱好游戏，动漫。闲来无事喜欢在一些不健康的场所虚度光阴，撸起代码就会废寝忘食。</p>
         </div>
         <div class="section3">
+            <div class="footer-top">
+                <div class="container">
+                    <div class="layui-row">
+                        <div class="layui-col-xs12">
+                            <h2>Mr.lzc</h2>
+                            <p>一个不知名的程序员</p>
+                            <router-link to="About">About Me</router-link>
+                        </div>
+                        <div class="layui-col-xs12">
+                            <h2>相关连接</h2>
+                            <ul>
+                                <li>
+                                    <router-link to="blog/0"><i class="el-icon-notebook-1"></i>博文</router-link>
+                                </li>
+                                <li>
+                                    <router-link to="message"><i class="el-icon-chat-dot-round"></i>留言</router-link>
+                                </li>
+                                <li>
+                                    <router-link to="/"><i class="el-icon-link"></i>资源</router-link>
+                                </li>
+                                <li>
+                                    <router-link to="diary"><i class="el-icon-document"></i>日记</router-link>
+                                </li>
+                                <li>
+                                    <router-link to="/"><i class="el-icon-edit"></i>归档</router-link>
+                                </li>
+                            </ul>
+                        </div>
+                        <div class="layui-col-xs12">
+                            <h2>联系我</h2>
+                            <ul>
+                                <li><i class="el-icon-notebook-1"></i>地址：北京市房山区阎村镇</li>
+                                <li><i class="el-icon-notebook-1"></i>QQ：604043484</li>
+                                <li><i class="el-icon-notebook-1"></i>邮箱：604043484@qq.com</li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="container">
 
+            </div>
         </div>
     </div>
 </template>
@@ -29,6 +70,8 @@
 
 <style scoped lang="less">
     .other{
+        position: absolute;
+        z-index: 4;
         width: 100%;
         height: 890px;
         background: url("../../assets/img/bgc2.jpg") center center/cover fixed;
@@ -127,7 +170,129 @@
         .section3{
             width: 100%;
             height: 344px;
-            background-color: rgb(35, 35, 40);;
+            background-color: rgb(35, 35, 40);
+
+            .footer-top{
+                width: 100%;
+                height: 100%;
+                box-sizing: border-box;
+                padding: 60px 0 90px;
+
+                .container{
+                    height: 100%;
+                    width: 90%;
+                    margin: 0 auto;
+
+                    .layui-row{
+                        width: 100%;
+                        height: 100%;
+
+                        .layui-col-xs12{
+                            width: 33.3333333333%;
+                            height: 100%;
+                            float: left;
+
+                            &:nth-child(1){
+                                h2{
+                                    width: 100%;
+                                    margin: 0;
+                                    padding: 0;
+                                    color: #fff;
+                                    font-family: BarbaraHand;
+                                    font-size: 3em;
+                                    font-weight: 400;
+                                }
+                                p{
+                                    width: 100%;
+                                    color: #f8f9f7;
+                                    letter-spacing: 10px;
+                                    margin: 20px 0;
+                                }
+                                a{
+                                    margin-top: 20px;
+                                    display: inline-block;
+                                    height: 38px;
+                                    line-height: 38px;
+                                    padding: 0 18px;
+                                    background-color: rgba(30,144,255,1);
+                                    color: #fff;
+                                    white-space: nowrap;
+                                    text-align: center;
+                                    font-size: 14px;
+                                    border: none;
+                                    border-radius: 2px;
+                                    cursor: pointer;
+
+                                    &:hover{
+                                        background-color: rgba(30,144,255,0.8);
+                                    }
+                                }
+                            }
+
+                            &:nth-child(2){
+                                h2{
+                                    color: #fff;
+                                    font-size: 18px;
+                                    margin-bottom: 30px;
+                                    font-weight: normal;
+                                    text-transform: uppercase;
+                                }
+
+                                ul{
+                                    li{
+                                        float: left;
+                                        width: 50%;
+                                        margin-bottom: 10px;
+                                        color: #fff;
+
+                                        a{
+                                            color: #fff;
+                                            font-size: 15px;
+
+                                            i{
+                                                margin-right: 10px;
+                                            }
+
+                                            &:hover{
+                                                color: rgb(107, 195, 13);
+
+                                                i{
+                                                    color: rgb(107, 195, 13)
+                                                }
+                                            }
+                                        }
+
+
+                                    }
+                                }
+                            }
+
+                            &:nth-child(3){
+                                h2{
+                                    color: #fff;
+                                    font-size: 18px;
+                                    margin-bottom: 30px;
+                                    font-weight: normal;
+                                    text-transform: uppercase;
+                                }
+
+                                ul {
+                                    li {
+                                        width: 50%;
+                                        margin-bottom: 10px;
+                                        color: #fff;
+                                        font-size: 15px;
+
+                                        i{
+                                            margin-right: 10px;
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
         }
     }
 </style>
